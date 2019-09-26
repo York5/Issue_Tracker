d5edbaf8923541bb6be0ff8b6b5560a63f0cb2cf
York5 <mikhailvishin2@yandex.com>
2019-09-27 01:11:53 +0600

    example pages templates added. 1st migration

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Issue(models.Model):
+    summary = models.CharField(max_length=100, null=False, blank=False, verbose_name='Name')
+    description = models.TextField(max_length=2000, null=True, blank=True, verbose_name='Description')
+    status = models.ForeignKey('Status', on_delete=models.PROTECT, null=False, blank=False, verbose_name='Status',
+                               related_name='issues')
+    type = models.ForeignKey('Type', on_delete=models.PROTECT, null=False, blank=False, verbose_name='Status',
+                             related_name='issues')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Time Created')
+
+    def __str__(self):
+        return self.summary
+
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=20, verbose_name='Name')
+
+
+class Type(models.Model):
+    type_name = models.CharField(max_length=20, verbose_name='Name')
