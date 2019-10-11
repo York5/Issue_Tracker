59b83c0af73921487db669358cac3df15cde1a0e
York5 <mikhailvishin2@yandex.com>
2019-10-11 12:52:09 +0600

    project template updated to include colors for different statuses

--- source/webapp/views/base_views.py
+++ source/webapp/views/base_views.py
@@ -5,28 +5,28 @@ from django.views.generic import TemplateView
 
 
 
-class DeleteView(View):
-    model = None
-    key_kwarg = 'pk'
-    template_name = None
-    redirect_url = None
-    context_key = 'object'
-    confirm = True
-
-    def get(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
-        self.object = get_object_or_404(self.model, pk=pk)
-        if self.confirm:
-            return render(request, self.template_name, context={self.context_key: self.object})
-        else:
-            self.object.delete()
-            return redirect(self.redirect_url)
-
-    def post(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
-        self.object = get_object_or_404(self.model, pk=pk)
-        self.object.delete()
-        return redirect(self.redirect_url)
-
-    def get_redirect_url(self):
-        return self.redirect_url
+# class DeleteView(View):
+#     model = None
+#     key_kwarg = 'pk'
+#     template_name = None
+#     redirect_url = None
+#     context_key = 'object'
+#     confirm = True
+#
+#     def get(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk')
+#         self.object = get_object_or_404(self.model, pk=pk)
+#         if self.confirm:
+#             return render(request, self.template_name, context={self.context_key: self.object})
+#         else:
+#             self.object.delete()
+#             return redirect(self.redirect_url)
+#
+#     def post(self, request, *args, **kwargs):
+#         pk = kwargs.get('pk')
+#         self.object = get_object_or_404(self.model, pk=pk)
+#         self.object.delete()
+#         return redirect(self.redirect_url)
+#
+#     def get_redirect_url(self):
+#         return self.redirect_url
