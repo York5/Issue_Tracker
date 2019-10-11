7eac1e2711f66c9e4605acf35ccb701446e42089
York5 <mikhailvishin2@yandex.com>
2019-10-11 16:49:08 +0600

    Bonus task implemented. Issues can not be edited if their project is closed. Project page correctly displays issues assigned to it.

--- source/webapp/views/project_views.py
+++ source/webapp/views/project_views.py
@@ -1,7 +1,8 @@
 from django.core.paginator import Paginator
+from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from webapp.forms import ProjectForm
-from webapp.models import Project
+from webapp.models import Project, STATUS_CHOICES
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
@@ -10,7 +11,7 @@ class ProjectIndexView(ListView):
     context_object_name = 'projects'
     model = Project
     ordering = ['-created_at']
-    paginate_by = 3
+    paginate_by = 4
     paginate_orphans = 1
 
 
@@ -18,9 +19,22 @@ class ProjectView(DetailView):
     template_name = 'projects/project.html'
     model = Project
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        project = self.object
+        issues = project.issues.order_by('-created_at')
+        paginator = Paginator(issues, 3, 0)
+        page_number = self.request.GET.get('page', 1)
+        page = paginator.get_page(page_number)
+        context['paginator'] = paginator
+        context['page_obj'] = page
+        context['issues'] = page.object_list
+        context['is_paginated'] = page.has_other_pages()
+        return context
+
 
 class ProjectCreateView(CreateView):
-    form_class = Project
+    form_class = ProjectForm
     model = Project
     template_name = 'projects/project_create.html'
 
@@ -41,5 +55,11 @@ class ProjectUpdateView(UpdateView):
 class ProjectDeleteView(DeleteView):
     model = Project
     template_name = 'projects/project_delete.html'
-    context_object_name = 'project'
     success_url = reverse_lazy('project_index')
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        success_url = self.get_success_url()
+        self.object.status = 'closed'
+        self.object.save()
+        return HttpResponseRedirect(success_url)
