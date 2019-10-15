2017cf8f113209dace21d5fb32cdcba12e192c1b
York5 <mikhailvishin2@yandex.com>
2019-10-15 19:20:07 +0600

    Задание 2 первый запрос сделан был с использованием поля created_at. Не создал увы вовремя поле updated_at что было бы логичнейю

--- source/webapp/Homework_42_task_2.py
+++ source/webapp/Homework_42_task_2.py
@@ -3,7 +3,7 @@ from datetime import timedelta, datetime
 from webapp.models import Issue, Project, Status, Type
 from django.db.models import Q
 
-qs_1 = Issue.objects.filter(Q(status__status_name='Done')) & (Q(created_at__lte=datetime.today(), created_at__gt=datetime.today()-timedelta(days=30)))
+qs_1 = Issue.objects.filter(Q(status__status_name='Done') & Q(created_at__lte=datetime.today(), created_at__gte=datetime.today()-timedelta(days=30)))
 print(qs_1)
 qs_2 = Issue.objects.filter(project__name='Moon Gateway').values('type__type_name','project__name')
 print(qs_2)
