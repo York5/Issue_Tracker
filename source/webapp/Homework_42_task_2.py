0fd38c6624131ae935fcf7440cb405e3a0a37928
York5 <mikhailvishin2@yandex.com>
2019-10-15 19:07:38 +0600

    almost all task 2 works

--- source/webapp/Homework_42_task_2.py
+++ source/webapp/Homework_42_task_2.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 
 qs_1 = Issue.objects.filter(Q(status__status_name='Done')) & (Q(created_at__lte=datetime.today(), created_at__gt=datetime.today()-timedelta(days=30)))
 print(qs_1)
-qs_2 = Type.objects.filter()
+qs_2 = Issue.objects.filter(project__name='Moon Gateway').values('type__type_name','project__name')
 print(qs_2)
-qs_3 = Project.objects.filter(Q(issues__description__icontains=self.search_value))
+qs_3 = Issue.objects.filter(Q(project__description__icontains='new'))
 print(qs_3)
