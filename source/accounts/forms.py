4f62fbacc8511c49f867b74c99250f184ec05f08
York5 <mikhailvishin2@yandex.com>
2019-10-29 16:10:31 +0600

    Registration form and templates added and working.

---
+++ source/accounts/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.models import User
+from django import forms
+from django.core.exceptions import ValidationError
+
+
+class UserCreationForm(forms.ModelForm):
+    password = forms.CharField(label="Пароль", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+
+    def clean_password_confirm(self):
+        password = self.cleaned_data.get("password")
+        password_confirm = self.cleaned_data.get("password_confirm")
+        if password and password_confirm and password != password_confirm:
+            raise forms.ValidationError('Пароли не совпадают!')
+        return password_confirm
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        try:
+            User.objects.get(email=email)
+            raise ValidationError('Email already registered.', code='email_registered')
+        except User.DoesNotExist:
+            return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'password_confirm', 'first_name', 'last_name', 'email']
