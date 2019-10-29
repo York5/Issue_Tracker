c26f26b8388a73031b66276e83b981ed7bfafb7e
York5 <mikhailvishin2@yandex.com>
2019-10-29 16:49:29 +0600

    Form Validations added to check for email and first and last names.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -4,14 +4,15 @@ from django.core.exceptions import ValidationError
 
 
 class UserCreationForm(forms.ModelForm):
-    password = forms.CharField(label="Пароль", strip=False, widget=forms.PasswordInput)
-    password_confirm = forms.CharField(label="Подтвердите пароль", widget=forms.PasswordInput, strip=False)
+    password = forms.CharField(label="Password", strip=False, widget=forms.PasswordInput)
+    password_confirm = forms.CharField(label="Confirm Password", widget=forms.PasswordInput, strip=False)
+    email = forms.EmailField(label="Email Address", required=True)
 
     def clean_password_confirm(self):
         password = self.cleaned_data.get("password")
         password_confirm = self.cleaned_data.get("password_confirm")
         if password and password_confirm and password != password_confirm:
-            raise forms.ValidationError('Пароли не совпадают!')
+            raise forms.ValidationError('Passwords do not match!')
         return password_confirm
 
     def clean_email(self):
@@ -21,6 +22,13 @@ class UserCreationForm(forms.ModelForm):
             raise ValidationError('Email already registered.', code='email_registered')
         except User.DoesNotExist:
             return email
+
+    def clean_first_name(self):
+        first_name = self.cleaned_data.get('first_name')
+        last_name = self.cleaned_data.get('last_name')
+        if not first_name and not last_name:
+            raise forms.ValidationError('Fill out either First Name or Last Name!')
+        return first_name
 
     def save(self, commit=True):
         user = super().save(commit=False)
