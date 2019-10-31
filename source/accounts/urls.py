5333cb8ec95eb6d9300d46c4fd4de05c745feab1
York5 <mikhailvishin2@yandex.com>
2019-10-31 17:33:19 +0600

    Смена Пароля добавлена в личный кабинет.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from accounts.views import login_view, logout_view, register_view, UserDetailView, UserChangeView
+from accounts.views import login_view, logout_view, register_view, UserDetailView, UserChangeView, UserChangePasswordView
 
 urlpatterns = [
     path('login/', login_view, name='login'),
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
     path('<int:pk>/', UserDetailView.as_view(), name='user_detail'),
-    path('<int:pk>/update', UserChangeView.as_view(), name='user_update')
+    path('<int:pk>/update', UserChangeView.as_view(), name='user_update'),
+    path('<int:pk>/password_change', UserChangePasswordView.as_view(), name='user_change_password')
 ]
 
 app_name = 'accounts'
--- source/accounts/views.py
+++ source/accounts/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.generic import DetailView, UpdateView
 
-from .forms import UserCreationForm
+from .forms import UserCreationForm, UserChangePasswordForm
 
 
 def login_view(request):
@@ -57,3 +57,16 @@ class UserChangeView(UserPassesTestMixin, UpdateView):
 
     def get_success_url(self):
         return reverse('accounts:user_detail', kwargs={'pk': self.object.pk})
+
+
+class UserChangePasswordView(UserPassesTestMixin, UpdateView):
+    model = User
+    template_name = 'user_change_password.html'
+    form_class = UserChangePasswordForm
+    context_object_name = 'user_obj'
+
+    def test_func(self):
+        return self.get_object() == self.request.user
+
+    def get_success_url(self):
+        return reverse('accounts:login')
